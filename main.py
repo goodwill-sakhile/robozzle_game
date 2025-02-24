e17f73a7b65f4d133dceb1f1d58d86c39f04cae4
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-02-24 13:13:21 -0800

    main script

--- main.py
+++ main.py
@@ -4,6 +4,84 @@ from kivymd.uix.gridlayout import MDGridLayout
 from kivy.lang import Builder
 from touch import TouchBox
 root = Builder.load_string("""
+
+<BlueColorButton>:
+    size_hint:None, None
+    size:"50dp", "50dp"
+    md_bg_color:30/float(255), 144/float(255), 255/float(255), 1
+<OrangeColorButton>:
+    size_hint:None, None
+    size:"50dp", "50dp"
+    md_bg_color: 255/float(255), 165/float(255), 0/float(255), 1
+<GreenColorButton>:
+    size_hint:None, None
+    size:"50dp", "50dp"
+    md_bg_color:0/float(255), 255/float(255), 127/float(255), 1
+<ControlsSpace>:
+    id:control_space
+    size_hint:None, None
+    size:"120dp", "206dp"
+    MDBoxLayout:
+        size_hint_x:None
+        width:"50dp"
+        orientation:"vertical"
+        space:2
+        MDIconButton:
+            size_hint:None, None
+            size:"50dp", "50dp"
+            icon:"arrow-up-thin"
+        MDIOconButton:
+            size_hint:None, None
+            size:"50dp", "50dp"
+            icon:"arrow-left-thin"
+        MDIconButton:
+            size_hint:None, None
+            size:"50dp", "50d0p"
+            icon:"arrow-right-thin"
+        MDBoxLayout:
+            md_bg_color:120/float(255), 120/float(255), 120/float(255), 1
+            size_hint:None, None
+            size:"50dp", "50dp"
+            MDLabel:
+                text:"F1"
+                text_size:self.size
+                halign:"center"
+                valign:"middle"
+                color:0, 0, 0, 1
+                bold:True
+    MDBoxLayout:
+        size_hint_x:None
+        width:"20dp"
+    MDBoxLayout:
+        space:2
+        Widget:
+        BlueColorButton:
+            root:control_space
+        OrangeColorButton:
+            root:control_space
+        GreenColorButton:
+            root:control_space
+        Widget:
+<ExecutionCommands>:
+    size_hint_y:None
+    width:"258dp"
+    spacing:2
+    MDIconButton:
+        size_hint:None, None
+        size:"50dp", "50dp"
+        icon:"play"
+    MDIconButton:
+        size_hint:None, None
+        size:"50dp", "50dp"
+        icon:"play-pause"
+    MDIconButton:
+        size_hint:None, None
+        size:"50dp", "50dp"
+        icon:"stop"
+    MDIconButton;
+        size_hint:None, None
+        size:"50dp", "50dp"
+        icon:"window-close"
 <SpeedButtonBox>:
     id:speed_button_box
     size_hint:None, None
@@ -55,14 +133,27 @@ root = Builder.load_string("""
         BlocksSpace:
         MDBoxLayout:
     MDBoxLayout:
-        size_hint_y:None
         GameSpeed:
+        MDBoxLayout:
+            size_hint_x:None
+            width:"200dp"
+        ExecutionCommands:
         Widget:
-        
 """)
+class BlueColorButton(TouchBox):
+    def respondToTouch(self):
+        pass
+class OrangeColorButton(TouchBox):
+    def respondToTouch(self):
+        pass
+class GreenColorButton(TouchBox):
+    def respondToTouch(self):
+        pass
+class ExecutionCommands(MDBoxLayout):
+    pass
 class SpeedButtonBox(TouchBox):
     def respondToTouch(self):
-        
+        pass
 class GameSpeed(MDBoxLayout):
     pass
 class Block(MDBoxLayout):
