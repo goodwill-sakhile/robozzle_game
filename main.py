878970b81cbb351460dff5c7c487fe3e5139a979
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-02-25 13:01:33 -0800

    main script

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import os
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 from kivy.lang import Builder
 from touch import TouchBox
 root = Builder.load_string("""
-
+<InstructionBox>:
+    id:instruction_box
+    size_hint:None, None
+    size:"50dp", "50dp"
+    md_bg_color:170/float(255), 170/float(255), 170/float(255), 1
+    padding:3
+    MDBoxLayout:
+        root:instruction_box
+        id:instruction
+        md_bg_color:170/float(255), 170/float(255), 170/float(255), 1
+<FunctionsBasicSpace>:
+    orientation:"vertical"
+    spacing:3
+    size_hint:None, None
+    size:"210dp", "100dp"
+    MDBoxLayout:
+    MDBoxLayout:
+        size_hint_y:None
+        height:"50dp"
+        MDBoxLayout:
+            size_hint:None, None
+            size:"50dp", "50dp"
+            MDLabel:
+                text:"F1"
+                text_size:self.size
+                halign:"center"
+                valign:"middle"
+                color:0, 0, 0, 1
+        InstructionBox:
+        InstructionBox:
+    MDBoxLayout:
 <BlueColorButton>:
     size_hint:None, None
     size:"50dp", "50dp"
@@ -20,17 +52,17 @@ root = Builder.load_string("""
 <ControlsSpace>:
     id:control_space
     size_hint:None, None
-    size:"120dp", "206dp"
+    size:"170dp", "206dp"
     MDBoxLayout:
         size_hint_x:None
         width:"50dp"
         orientation:"vertical"
-        space:2
+        spacing:2
         MDIconButton:
             size_hint:None, None
             size:"50dp", "50dp"
             icon:"arrow-up-thin"
-        MDIOconButton:
+        MDIconButton:
             size_hint:None, None
             size:"50dp", "50dp"
             icon:"arrow-left-thin"
@@ -53,7 +85,9 @@ root = Builder.load_string("""
         size_hint_x:None
         width:"20dp"
     MDBoxLayout:
-        space:2
+        size_hint_x:None
+        width:"50dp"
+        spacing:2
         Widget:
         BlueColorButton:
             root:control_space
@@ -78,29 +112,35 @@ root = Builder.load_string("""
         size_hint:None, None
         size:"50dp", "50dp"
         icon:"stop"
-    MDIconButton;
+    MDIconButton:
         size_hint:None, None
         size:"50dp", "50dp"
+        theme_text_color:"Custom"
+        md_bg_color:0, 1, 0, 1
         icon:"window-close"
 <SpeedButtonBox>:
+    text:""
     id:speed_button_box
     size_hint:None, None
     size:"50dp", "50dp"
+    md_bg_color:0, 0, 0, 1
     MDLabel:
         id:speed_number
-        text:"x1"
+        text:root.text
         text_size:self.size
         halign:"center"
         valign:"middle"
         color:1, 1, 1, 1
 <GameSpeed>:
     id:game_speed_box
-    size_hint:None, None
-    size:"70dp", "160dp"
+    size_hint_y:None
+    height:"70dp"
     orientation:"vertical"
-    MDBoxLatyout:
+    padding:"10dp", "0dp"
+    MDBoxLayout:
         size_hint_y:None
         height:"20dp"
+        padding:"5dp", "0dp"
         MDLabel:
             text:"Game Speed"
             text_size:self.size
@@ -108,19 +148,20 @@ root = Builder.load_string("""
             valign:"middle"
             color:0, 0, 0, 1
     MDBoxLayout:
+        size_hint_y:None
+        height:"50dp"
         SpeedButtonBox:
-            root:game_speed_box
+            text:"x1"
         SpeedButtonBox:
-            root:game_speed_box
-            self.ids.speed_number.text = "x2"
+            text:"x2"
         SpeedButtonBox:
-            root:game_speed_box
-            self.ids.speed_number.text = "x3"
+            text:"x3"
 <Block>:
     size_hint:None, None
     size:"50dp", "50dp"
-    md_bg_color: 90/float(255), 90/float(255), 90/float(255), 1
+    md_bg_color: 0/float(255), 0/float(255), 0/float(255), 1
 <BlocksSpace>:
+    md_bg_color:0, 1, 0, 1
     size_hint:None, None
     size:"800dp", "800dp"
     BlocksGrid:
@@ -131,15 +172,20 @@ root = Builder.load_string("""
         size_hint_y:None
         hieght:"800dp"
         BlocksSpace:
-        MDBoxLayout:
+
     MDBoxLayout:
+        size_hint_y:None
+        height:"100dp"
+        md_bg_color:0, 0, 1, 1
         GameSpeed:
-        MDBoxLayout:
-            size_hint_x:None
-            width:"200dp"
-        ExecutionCommands:
         Widget:
+        ExecutionCommands:
 """)
+class InstructionBox(TouchBox):
+    def respondToTouch(self):
+        pass
+class FunctionsBasicSpace(MDBoxLayout):
+    pass
 class BlueColorButton(TouchBox):
     def respondToTouch(self):
         pass
@@ -163,9 +209,9 @@ class BlocksSpace(MDBoxLayout):
 class BlocksGrid(MDGridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cols = 8
-        self.rows = 8
-        for i range(50):
+        self.cols = 16
+        self.rows = 16
+        for i in range(50):
             block = Block()
             self.add_widget(block)
 class MainGameSpace(MDBoxLayout):
